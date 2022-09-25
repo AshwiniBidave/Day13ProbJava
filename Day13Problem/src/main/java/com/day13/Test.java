e1194239a92ca9bac0bf7a4894ce8255029ef218
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-09-25 13:56:54 +0530

    Refractor1 implemented

--- Day13Problem/src/main/java/com/day13/Test.java
+++ Day13Problem/src/main/java/com/day13/Test.java
@@ -6,44 +6,62 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class Test 
+public class Test <T>
 {
 
-	public static String findmaxstrimg(String a, String b, String c)
+
+	public T a,b,c;
+	public Test(T a, T b, T c) {
+		this.a=a;
+		this.b=b;
+		this.c=c;
+	}
+	
+	public static <T extends Comparable<T>> T findmaxstring(T a, T b, T c)
     {
-		String maximum=a;
-        if (a.compareTo(b) > 0 && a.compareTo(c) > 0)
+		T maximum=a;
+        if (b.compareTo(maximum) > 0)
         {
-            return a;
+            maximum=b;
 
         }
 
 
-        if (b.compareTo(a) > 0 && b.compareTo(c) > 0)
+        if (c.compareTo(maximum) > 0)
         {
-            return b;
+            maximum=c;
 
         }
-        if (c.compareTo(a) > 0 && c.compareTo(b) > 0)
-        {
-            return c;
 
-        }
-		return maximum;
+showmaxvalue(a,b,c,maximum);
+	return maximum;
+	
+    }
+public static <T> void showmaxvalue(T a,T b,T c,T maximum) {
+	System.out.println("Maximum String is :"+maximum);	
+	
+	
+	
+	
+}
+
+
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+    System.out.println("Enter first String: ");
+    String a=s.next();
+    System.out.println("Enter Second Number: ");
+    String b=s.next();
+    System.out.println("Enter Third Number: ");
+    String c=s.next();
+   findmaxstring(a, b, c);
+  showmaxvalue();
+    
+
 	}
-	public static void main(String[] args)
-	{
-	 Scanner s=new Scanner(System.in);
-	 System.out.println("Enter first String: ");
-	 String a=s.next();
-	 System.out.println("Enter Second Number: ");
-	 String b=s.next();
-	 System.out.println("Enter Third Number: ");
-	 String c=s.next();
-	 String  output=Test.findmaxstrimg(a, b, c);
-     System.out.println("Maximum String is :"+output);
 
-		        
-	}
+private static void showmaxvalue() {
+	
+}
 
 }
