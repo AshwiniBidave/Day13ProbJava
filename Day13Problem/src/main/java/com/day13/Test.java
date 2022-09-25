632b7fb1627e39faed364422b1621bccd48b348b
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-09-25 14:05:39 +0530

    Refractor1 implemented

--- Day13Problem/src/main/java/com/day13/Test.java
+++ Day13Problem/src/main/java/com/day13/Test.java
@@ -7,46 +7,35 @@ import java.util.Scanner;
  *
  */
 public class Test <T>
-{
-
-
-	public T a,b,c;
-	public Test(T a, T b, T c) {
-		this.a=a;
-		this.b=b;
-		this.c=c;
-	}
-	
-	public static <T extends Comparable<T>> T findmaxstring(T a, T b, T c)
-    {
-		T maximum=a;
-        if (b.compareTo(maximum) > 0)
-        {
-            maximum=b;
-
+      {
+        public T a,b,c;
+	    public Test(T a, T b, T c)
+	    {
+	     this.a=a;
+		 this.b=b;
+		 this.c=c;
         }
-
-
-        if (c.compareTo(maximum) > 0)
+	    public static <T extends Comparable<T>> T findmaxstring(T a, T b, T c)
         {
-            maximum=c;
+	    	T maximum=a;
+	        if (b.compareTo(maximum) > 0)
+	        {
+	            maximum=b;
+	        }
+	        if (c.compareTo(maximum) > 0)
+	        {
+	            maximum=c;
+	        }
+	        showmaxvalue(a,b,c,maximum);
+	    	return maximum;
+	
+		 }
+         public static <T> void showmaxvalue(T a,T b,T c,T maximum)
+       {
+	       System.out.println("Maximum String is :"+maximum);	
+	    }
 
-        }
-
-showmaxvalue(a,b,c,maximum);
-	return maximum;
-	
-    }
-public static <T> void showmaxvalue(T a,T b,T c,T maximum) {
-	System.out.println("Maximum String is :"+maximum);	
-	
-	
-	
-	
-}
-
-
-public static void main(String[] args) {
+    public static void main(String[] args) {
 	Scanner s=new Scanner(System.in);
     System.out.println("Enter first String: ");
     String a=s.next();
@@ -54,8 +43,8 @@ public static void main(String[] args) {
     String b=s.next();
     System.out.println("Enter Third Number: ");
     String c=s.next();
-   findmaxstring(a, b, c);
-  showmaxvalue();
+    findmaxstring(a, b, c);
+    showmaxvalue();
     
 
 	}
