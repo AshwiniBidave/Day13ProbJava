ce0c9cbc08927ecaab42337ced1ecce54d87cb80
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-09-25 13:23:10 +0530

    Find2ndumMax  implemented

--- Day13Problem/src/main/java/com/day13/Test.java
+++ Day13Problem/src/main/java/com/day13/Test.java
@@ -1,5 +1,7 @@
 package com.day13;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,22 @@ public class Test
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter first Number: ");
+        int a=s.nextInt();
+        System.out.println("Enter Second Number: ");
+        int b=s.nextInt();
+        System.out.println("Enter Third Number: ");
+        int c=s.nextInt();
+        if(b>a && b>c) 
+        {
+        	System.out.println("Second number is maximum :"+b);
+        }else 
+        {
+        	System.out.println("other number is maximum not second number");
+        }
+        	
+     
     }
 }
