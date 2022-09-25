3fedb1274fa97abe61a028e2bb035d7d792f6208
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-09-25 14:14:10 +0530

    Refractor1 implemented

--- Day13Problem/src/main/java/com/day13/Test.java
+++ Day13Problem/src/main/java/com/day13/Test.java
@@ -9,48 +9,47 @@ import java.util.Scanner;
 public class Test <T>
       {
         public T a,b,c;
-	    public Test(T a, T b, T c)
+	public Test(T a, T b, T c)
 	    {
 	     this.a=a;
-		 this.b=b;
-		 this.c=c;
-        }
-	    public static <T extends Comparable<T>> T findmaxstring(T a, T b, T c)
+	     this.b=b;
+	     this.c=c;
+            }
+	public static <T extends Comparable<T>> T findmaxstring(T a, T b, T c)
         {
-	    	T maximum=a;
-	        if (b.compareTo(maximum) > 0)
-	        {
-	            maximum=b;
-	        }
-	        if (c.compareTo(maximum) > 0)
-	        {
-	            maximum=c;
-	        }
-	        showmaxvalue(a,b,c,maximum);
-	    	return maximum;
-	
-		 }
-         public static <T> void showmaxvalue(T a,T b,T c,T maximum)
-       {
-	       System.out.println("Maximum String is :"+maximum);	
-	    }
-
-    public static void main(String[] args) {
-	Scanner s=new Scanner(System.in);
-    System.out.println("Enter first String: ");
-    String a=s.next();
-    System.out.println("Enter Second Number: ");
-    String b=s.next();
-    System.out.println("Enter Third Number: ");
-    String c=s.next();
-    findmaxstring(a, b, c);
-    showmaxvalue();
-    
-
+	  T maximum=a;
+	  if (b.compareTo(maximum) > 0)
+	  {
+	   maximum=b;
+	  }
+	  if (c.compareTo(maximum) > 0)
+	  {
+	   maximum=c;
+	  }
+	   showmaxvalue(a,b,c,maximum);
+	   return maximum;
 	}
+         public static <T> void showmaxvalue(T a,T b,T c,T maximum)
+        {
+	  System.out.println("Maximum String is :"+maximum);	
+	 }
 
-private static void showmaxvalue() {
+    public static void main(String[] args) 
+   {
+	Scanner s=new Scanner(System.in);
+        System.out.println("Enter first String: ");
+        String a=s.next();
+        System.out.println("Enter Second Number: ");
+        String b=s.next();
+        System.out.println("Enter Third Number: ");
+        String c=s.next();
+        findmaxstring(a, b, c);
+        showmaxvalue();
+    }
+
+    private static void showmaxvalue()
+   {
 	
-}
+    }
 
 }
