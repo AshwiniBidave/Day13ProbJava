96224cc46c9e221043f466401dfcdeffff8df0fa
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-09-25 13:29:01 +0530

    FindMaxString implemented

--- Day13Problem/src/main/java/com/day13/Test.java
+++ Day13Problem/src/main/java/com/day13/Test.java
@@ -8,24 +8,42 @@ import java.util.Scanner;
  */
 public class Test 
 {
-    public static void main( String[] args )
-    {
 
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter first Number: ");
-        int a=s.nextInt();
-        System.out.println("Enter Second Number: ");
-        int b=s.nextInt();
-        System.out.println("Enter Third Number: ");
-        int c=s.nextInt();
-        if(b>a && b>c) 
+	public static String findmaxstrimg(String a, String b, String c)
+    {
+		String maximum=a;
+        if (a.compareTo(b) > 0 && a.compareTo(c) > 0)
         {
-        	System.out.println("Second number is maximum :"+b);
-        }else 
-        {
-        	System.out.println("other number is maximum not second number");
+            return a;
+
         }
-        	
-     
-    }
+
+
+        if (b.compareTo(a) > 0 && b.compareTo(c) > 0)
+        {
+            return b;
+
+        }
+        if (c.compareTo(a) > 0 && c.compareTo(b) > 0)
+        {
+            return c;
+
+        }
+		return maximum;
+	}
+	public static void main(String[] args)
+	{
+	 Scanner s=new Scanner(System.in);
+	 System.out.println("Enter first String: ");
+	 String a=s.next();
+	 System.out.println("Enter Second Number: ");
+	 String b=s.next();
+	 System.out.println("Enter Third Number: ");
+	 String c=s.next();
+	 String  output=Test.findmaxstrimg(a, b, c);
+     System.out.println("Maximum String is :"+output);
+
+		        
+	}
+
 }
